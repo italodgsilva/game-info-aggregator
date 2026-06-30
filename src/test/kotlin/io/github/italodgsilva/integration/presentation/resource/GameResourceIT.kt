91e65862e8cdbfb5577a0858501f607c3888297b
italodgsilva <italodellagarza@gmail.com>
2026-06-30 18:33:23 -0300

    fix: Testes com compilação nativa removidos

--- src/test/kotlin/io/github/italodgsilva/integration/presentation/resource/GameResourceIT.kt
+++
@@ -1,6 +0,0 @@
-package io.github.italodgsilva.integration.presentation.resource
-
-import io.quarkus.test.junit.QuarkusIntegrationTest
-
-@QuarkusIntegrationTest
-class GameResourceIT : GameResourceTest()
